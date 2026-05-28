bda5b36aedf85b0d97188c988f837e29f606c1bc
saavlean <65791583+saavlean@users.noreply.github.com>
2026-05-28 23:15:35 +0000

    fix csp header

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 'use strict';
+
 require('dotenv').config();
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -9,31 +11,34 @@ const runner = require('./test-runner');
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.setHeader("Content-Security-Policy", "default-src 'self'; script-src 'self'; style-src 'self'");
+app.use(function (req, res, next) {
+  res.setHeader(
+    'Content-Security-Policy',
+    "default-src 'self'; script-src 'self'; style-src 'self';"
+  );
   next();
-  });
+});
 
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(cors({ origin: '*' }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.route('/').get(function(req, res) {
+app.route('/').get(function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 fccTestingRoutes(app);
 apiRoutes(app);
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.status(404).type('text').send('Not Found');
 });
 
-const listener = app.listen(process.env.PORT || 3000, function() {
+const listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
   console.log('Running Tests...');
-  setTimeout(function() {
+  setTimeout(function () {
     try {
       runner.run();
     } catch (e) {
